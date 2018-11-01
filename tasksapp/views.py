94ecf2248fdbe771ea63836dff30bfc514e4f1b0
areumcho <cho00052@algonquinlive.com>
2018-11-01 03:56:53 -0400

    initial commit

---
+++ tasksapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from tasksapp.models import Task
+from django.utils import timezone
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+
+def index(request):
+	if request.method == 'GET':
+		objects = Task.objects.all().order_by('duedate')
+		return render(request, "index.html", {'tasks':objects})
+	elif request.method == 'POST':
+		taskName = request.POST.get("task","")
+		taskDesc = request.POST.get("desc","")
+		assignedTo = request.POST.get("assignee","")
+		date = request.POST.get("duedate","")
+		reporterName = request.POST.get("reporter","")
+		state = request.POST.get("status","")
+		record = Task(name=taskName, description=taskDesc, assignee=assignedTo, duedate = date, 
+					reporter = reporterName, status = state, 
+					updatedate = timezone.now())
+		record.save()
+		return redirect('/')
+
+
+def fundraising(request):
+	return render(request, 'fundraising.html')
+
